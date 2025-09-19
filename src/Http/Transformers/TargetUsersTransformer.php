23c48a29b3b83420ebeee4e694a9ba4764a23d47
IBRAHIM KHALIL ATTEIB YACOUB <i.khalilatteib@gmail.com>
2025-09-19 20:15:24 +0300

    Refactor target users service to use crm_user_id and update caching logic

--- src/Http/Transformers/TargetUsersTransformer.php
+++ src/Http/Transformers/TargetUsersTransformer.php
@@ -4,9 +4,12 @@ namespace NextDeveloper\CRM\Http\Transformers;
 
 use Illuminate\Support\Facades\Cache;
 use NextDeveloper\Commons\Common\Cache\CacheHelper;
+use NextDeveloper\CRM\Database\Models\Targets;
 use NextDeveloper\CRM\Database\Models\TargetUsers;
 use NextDeveloper\Commons\Http\Transformers\AbstractTransformer;
+use NextDeveloper\CRM\Database\Models\Users;
 use NextDeveloper\CRM\Http\Transformers\AbstractTransformers\AbstractTargetUsersTransformer;
+use Psr\SimpleCache\InvalidArgumentException;
 
 /**
  * Class TargetUsersTransformer. This class is being used to manipulate the data we are serving to the customer
@@ -20,21 +23,36 @@ class TargetUsersTransformer extends AbstractTargetUsersTransformer
      * @param TargetUsers $model
      *
      * @return array
+     * @throws InvalidArgumentException
      */
     public function transform(TargetUsers $model)
     {
+
+        $crmTargetId = Targets::where('id', $model->crm_target_id)->first()?->uuid;
+        $crmUserId = Users::where('id', $model->crm_user_id)->first()?->uuid;
+
+
+        $cacheKey = $crmUserId . '_' . $crmTargetId;
+
         $transformed = Cache::get(
-            CacheHelper::getKey('TargetUsers', $model->uuid, 'Transformed')
+            CacheHelper::getKey('TargetUsers', $cacheKey, 'Transformed')
         );
 
         if($transformed) {
             return $transformed;
         }
 
-        $transformed = parent::transform($model);
+        $transformed = $this->buildPayload(
+            [
+                'crm_target_id'  =>  $crmTargetId,
+                'crm_user_id'  =>  $crmUserId,
+                'created_at'  =>  $model->created_at,
+                'updated_at'  =>  $model->updated_at,
+            ]
+        );
 
         Cache::set(
-            CacheHelper::getKey('TargetUsers', $model->uuid, 'Transformed'),
+            CacheHelper::getKey('TargetUsers', $cacheKey, 'Transformed'),
             $transformed
         );
 
