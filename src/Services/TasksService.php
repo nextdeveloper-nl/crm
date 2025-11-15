2cb0a90a70e14b9ccdb5dc3096f6f165e5b9b3d5
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-11-15 14:48:14 +0300

    Tasks service is updated

--- src/Services/TasksService.php
+++ src/Services/TasksService.php
@@ -2,6 +2,7 @@
 
 namespace NextDeveloper\CRM\Services;
 
+use App\Helpers\ObjectHelper;
 use NextDeveloper\CRM\Services\AbstractServices\AbstractTasksService;
 
 /**
@@ -15,4 +16,16 @@ class TasksService extends AbstractTasksService
 {
 
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+
+    public static function create($data)
+    {
+        if(array_key_exists('object_type', $data) && array_key_exists('object_id', $data)) {
+            $object = ObjectHelper::getObject($data['object_type'], $data['object_id']);
+
+            $data['object_type'] = get_class($object);
+            $data['object_id'] = $object->id;
+        }
+
+        return parent::create($data);
+    }
 }
