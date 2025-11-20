26060121b7dca2f8ed9d3522a39ad0aea409738b
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-11-20 20:47:25 +0300

    Accounts manager service updated

--- src/Services/AccountManagersService.php
+++ src/Services/AccountManagersService.php
@@ -2,7 +2,12 @@
 
 namespace NextDeveloper\CRM\Services;
 
+use NextDeveloper\Communication\Helpers\Communicate;
+use NextDeveloper\CRM\Database\Models\AccountManagers;
 use NextDeveloper\CRM\Services\AbstractServices\AbstractAccountManagersService;
+use NextDeveloper\IAM\Database\Scopes\AuthorizationScope;
+use NextDeveloper\IAM\Helpers\UserHelper;
+use NextDeveloper\CRM\Database\Models\AccountsPerspective;
 
 /**
  * This class is responsible from managing the data for AccountManagers
@@ -15,4 +20,30 @@ class AccountManagersService extends AbstractAccountManagersService
 {
 
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+
+    public static function create($data) {
+        //  Checking if we have a duplicate
+        $duplicate = AccountManagers::withoutGlobalScope(AuthorizationScope::class)
+            ->where([
+                'crm_account_id' => $data['crm_account_id'],
+                'iam_account_id' => $data['iam_account_id'],
+                'iam_user_id' => $data['iam_user_id'],
+            ])
+            ->first();
+
+        if($duplicate) {
+            return $duplicate;
+        }
+
+        $user = UserHelper::getUserWithId($data['iam_user_id']);
+
+        $crmAccount = AccountsPerspective::where('id', $data['crm_account_id'])->first();
+
+        (new Communicate($user))->sendNotification(
+            subject: 'Assigned as account manager',
+            message: 'You are assigned as an account manager to the account namely:' . $crmAccount->name
+        );
+
+        return parent::create($data);
+    }
 }
