fb4682ac09b6646c754140ea34b6537c8292cdce
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-11-21 19:04:09 +0300

    Sales person roles and SDR roles updated

--- src/Authorization/Roles/SalesDevelopmentRepresentative.php
+++ src/Authorization/Roles/SalesDevelopmentRepresentative.php
@@ -87,60 +87,9 @@ class SalesDevelopmentRepresentative extends AbstractRole implements IAuthorizat
 
     public function allowedOperations() :array
     {
-        return [
-            'crm_accounts:read',
-            'crm_accounts:create',
-            'crm_accounts:update',
-            'crm_users:read',
-            'crm_users:create',
-            'crm_users:update',
-            'crm_account_managers:read',
-            'crm_account_perspectives:read',
-            'crm_account_users_perspective:read',
-            'crm_ideal_customer_profiles:read',
-            'crm_ideal_customer_profiles:create',
-            'crm_ideal_customer_profiles:update',
-            'crm_ideal_customer_profiles:delete',
-            'crm_opportunities:read',
-            'crm_opportunities:create',
-            'crm_opportunities:update',
-            'crm_user_managers:read',
-            'crm_user_managers:create',
-            'crm_user_managers:update',
-            'crm_users_perspective:read',
-            'crm_quotes:read',
-            'crm_quotes:create',
-            'crm_quotes:update',
-            'crm_quote_items:read',
-            'crm_quote_items:create',
-            'crm_quote_items:update',
-            'crm_quote_items:delete',
-            'crm_opportunities:read',
-            'crm_opportunities:create',
-            'crm_opportunities:update',
-            'crm_calls:read',
-            'crm_calls:create',
-            'crm_calls:update',
-            'crm_calls:delete',
-            'crm_emails:read',
-            'crm_emails:create',
-            'crm_emails:update',
-            'crm_emails:delete',
-            'crm_tasks:read',
-            'crm_tasks:create',
-            'crm_tasks:update',
-            'crm_tasks:delete',
-            'crm_notes:read',
-            'crm_notes:create',
-            'crm_notes:update',
-            'crm_notes:delete',
-            'crm_meetings:read',
-            'crm_meetings:create',
-            'crm_meetings:update',
-            'crm_meetings:delete',
+        return array_merge((new SalesPersonRole())->allowedOperations(), [
 
-            'crm_accounts_perspective:read',
-        ];
+        ]);
     }
 
     public function canBeApplied($column)
