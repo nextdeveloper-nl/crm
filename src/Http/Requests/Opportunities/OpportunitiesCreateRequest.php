20d820c59124c48165e2e2801bb7a6b150c0fcaa
Harun Baris Bulut <baris.bulut@plusclouds.com>
2026-02-23 23:39:54 +0300

    reason_lost and common_currency_id added to opportunities

--- src/Http/Requests/Opportunities/OpportunitiesCreateRequest.php
+++ src/Http/Requests/Opportunities/OpportunitiesCreateRequest.php
@@ -14,15 +14,17 @@ class OpportunitiesCreateRequest extends AbstractFormRequest
     {
         return [
             'name' => 'required|string',
-        'description' => 'nullable|string',
-        'probability' => 'integer',
-        'source' => 'nullable|string',
-        'income' => '',
-        'deadline' => 'nullable|date',
-        'crm_account_id' => 'required|exists:crm_accounts,uuid|uuid',
-        'tags' => '',
-        'opportunity_stage' => '',
-        'type' => 'string',
+            'description' => 'nullable|string',
+            'probability' => 'integer',
+            'source' => 'nullable|string',
+            'income' => '',
+            'deadline' => 'nullable|date',
+            'crm_account_id' => 'required|exists:crm_accounts,uuid|uuid',
+            'tags' => '',
+            'opportunity_stage' => '',
+            'type' => 'string',
+            'reason_lost' => 'nullable|string',
+            'common_currency_id' => 'required|exists:currencies,uuid|uuid',
         ];
     }
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
